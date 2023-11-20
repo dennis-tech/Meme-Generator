7a8fd3f91eab2770befe6484736eafc31641febc
dennis-tech <hedenniskibe@gmail.com>
2023-11-20 23:26:56 +0300

    Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header(){
+  return (
+    <header className='header'>
+        <img src="/public/troll-face.png"  className='header__image'/>
+        <h2 className='header__title'>Header component</h2>
+        <h4 className='header__project'>React Course - Project 3</h4>
+    </header>
+  )
+}
